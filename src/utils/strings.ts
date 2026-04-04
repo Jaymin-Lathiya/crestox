931399c739b1469c0bdd5f90bade4375eb214630
ADityaraj3 <adityasraj123@gmail.com>
2026-04-04 20:38:44 +0530

    [UPDATE] Change base_url to a static ngrok URL for development; comment out environment variable

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,4 @@
 export const strings = {
-    base_url: process.env.NEXT_PUBLIC_BASE_URL,
+    // base_url: process.env.NEXT_PUBLIC_BASE_URL,
+    base_url: "https://d1cc-2402-a00-10a-3e9a-c046-bd30-c9e2-f5f0.ngrok-free.app/api",
 };
